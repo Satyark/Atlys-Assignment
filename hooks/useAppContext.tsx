92c14e331f0f9eab687a7ba4d7a44cc615d10578
AhaanSehgal <ahaan.singh@neobase.one>
2024-08-16 23:47:21 +0530

    fix: fix hook return type

--- hooks/useAppContext.tsx
+++ hooks/useAppContext.tsx
@@ -3,5 +3,8 @@ import { useContext} from "react";
 
 export const useAppContext = ()=>{
   const context = useContext(AppContext);
+  if (context === undefined) {
+    throw new Error('useAppContext must be used within a Provider');
+  }
   return context;
 }
