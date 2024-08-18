99009f77a3221e0c990aeacb88ab046b21587ff6
AhaanSehgal <ahaan.singh@neobase.one>
2024-08-18 21:38:59 +0530

    feat: add wallet controls on layout and disconnect

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,16 +1,37 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Inter } from "next/font/google";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 import { PropsWithChildren } from "react";
+import { useAccount } from "wagmi";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Layout = ({ children }: PropsWithChildren) => {
+  
+  const router = useRouter()
+  const { isConnected, isDisconnected } = useAccount();
+
+  useEffect(()=>{
+    if(isDisconnected){
+      router.push("/", undefined, { shallow: true });
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[isDisconnected])
+
   return (
-    <div
-      className={`flex items-center justify-center w-full ${inter.className}`}
-    >
-      {children}
-    </div>
+    <>
+      {isConnected && (
+        <div className="w-full flex justify-end p-4">
+          <ConnectButton />
+        </div>
+      )}
+      <div
+        className={`flex items-center justify-center w-full ${inter.className}`}
+      >
+        {children}
+      </div>
+    </>
   );
 };
 
