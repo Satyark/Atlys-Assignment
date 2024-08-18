3b5fcc4415a8015a4af0ccd21456078e7ceec942
AhaanSehgal <ahaan.singh@neobase.one>
2024-08-18 21:22:23 +0530

    fix: add project id

--- rainbow-kit/index.ts
+++ rainbow-kit/index.ts
@@ -3,7 +3,7 @@ import { arbitrum, base, canto, mainnet, optimism, polygon } from "viem/chains";
 
 export const rainwbowKitConfig = getDefaultConfig({
   appName: "Web3 Feeds",
-  projectId: "",
+  projectId: "myprojectid", //TODO: add project id later once everything gets functional
   chains: [mainnet, polygon, optimism, arbitrum, base, canto],
   ssr: true,
 });
